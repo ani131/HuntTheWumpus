1036d4f8d9afafd5190236d91414eabd7a3d3f25
ani131 <anish.paspuleti@gmail.com>
2021-05-07 16:33:55 -0400

    Add files via upload

---
+++ Textfield.java
@@ -0,0 +1,7 @@
+//Add imports as needed
+
+public class Textfield extends MoveShoot { // class to display Text when near Wumpi/Pit/Bats
+  public Textfield() {
+    // Has access to all the variables in MoveShoot(including coords)
+  }
+}
