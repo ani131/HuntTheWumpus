979e64a6639a679b86caa12a77207708f3b4223c
ani131 <anish.paspuleti@gmail.com>
2021-05-07 13:14:49 -0400

    Update App.java

--- App.java
+++ App.java
@@ -3,11 +3,15 @@ import java.awt.event.*;
 
 public class App extends Frame implements KeyListener {
 
-    int x = 50, y = 60;
+    int moveX = 50, moveY = 60;
+    int shootX = moveX, shootY = moveY;
+    int wumpusX = 450, wumpusY = 60;
+
+    int numArrows = 4;
 
     public App() {
 
-        setSize(600, 600);
+        setSize(900, 600); // Initializing the JPanel including size, location, etc.
         setTitle("Hunt The Wumpus");
         setLocationRelativeTo(null);
 
@@ -23,9 +27,12 @@ public class App extends Frame implements KeyListener {
     }
 
     public void paint(Graphics g) {
-        g.drawString("x=" + (x - 50 + 100) / 100 + ", y=" + (y - 60 + 100) / 100, 40, 45);
+        g.drawString("x=" + (moveX - 50 + 100) + ", y=" + (moveY - 60 + 100), 40, 45); // A string that displays
+                                                                                       // coordinates at the top left
+                                                                                       // ex: (x=1, y=2)
+        g.drawString("wumpusX=" + (wumpusX - 50 + 100) + ", wumpusY=" + (wumpusY - 60 + 100), 200, 45);
 
-        g.drawLine(150, 60, 150, 560);
+        g.drawLine(150, 60, 150, 560); // horzontal and vertical grid lines
         g.drawLine(250, 60, 250, 560);
         g.drawLine(350, 60, 350, 560);
         g.drawLine(450, 60, 450, 560);
@@ -36,46 +43,114 @@ public class App extends Frame implements KeyListener {
         g.drawLine(50, 460, 550, 460);
 
         g.drawRect(50, 60, 500, 500);
-        g.setColor(Color.CYAN);
-        g.fillRect(x, y, 100, 100);
+        g.setColor(Color.CYAN); // Cyan colored character
+        g.fillRect(moveX, moveY, 100, 100);
 
     }
 
-    public void keyPressed(KeyEvent ke) {
+    void moveUp() {
+        if (moveY > 80) {
+            moveY = moveY - 100;
+            shootY = moveY;
+        }
+    }
+
+    void moveDown() {
+        if (moveY < 419) {
+            moveY = moveY + 100;
+            shootY = moveY;
+        }
+    }
+
+    void moveLeft() {
+        if (moveX > 100) {
+            moveX = moveX - 100;
+            shootX = moveX;
+        }
+    }
+
+    void moveRight() {
+        if (moveX < 389) {
+            moveX = moveX + 100;
+            shootX = moveX;
+        }
+    }
+
+    public void keyPressed(KeyEvent ke) { // movement
         int keyCode = ke.getKeyCode();
 
         switch (keyCode) {
             case KeyEvent.VK_W:
-                if (y > 80)
-                    y = y - 100;
+                moveUp();
                 break;
             case KeyEvent.VK_S:
-                if (y < 419)
-                    y = y + 100;
+                moveDown();
                 break;
             case KeyEvent.VK_A:
-                if (x > 100)
-                    x = x - 100;
+                moveLeft();
                 break;
             case KeyEvent.VK_D:
-                if (x < 389)
-                    x = x + 100;
+                moveRight();
                 break;
             case KeyEvent.VK_UP:
-                if (y > 80)
-                    y = y - 100;
+
+                System.out.println(shootY);
+                if ((shootY - 100) == wumpusY && (shootX == wumpusX)) {
+                    System.out.println("You hit the Wumpus");
+                    // add victory screen stuff here
+                } else {
+                    System.out.println("You did not hit the wumpus");
+                    // decrease num arrows
+                    numArrows -= 1;
+                    if (numArrows == 0) {
+                        System.out.println("You ran out of arrows");
+                    }
+                }
+                shootY = moveY;
                 break;
+
             case KeyEvent.VK_DOWN:
-                if (y < 419)
-                    y = y + 100;
+                if ((shootY + 100) == wumpusY && (shootX == wumpusX)) {
+                    System.out.println("You hit the Wumpus");
+                    // add victory screen stuff here
+                } else {
+                    System.out.println("You did not hit the wumpus");
+                    // decrease num arrows
+                    numArrows -= 1;
+                    if (numArrows == 0) {
+                        System.out.println("You ran out of arrows");
+                    }
+                }
+                shootY = moveY;
                 break;
             case KeyEvent.VK_LEFT:
-                if (x > 100)
-                    x = x - 100;
+                if ((shootY == wumpusY) && ((shootX - 100) == wumpusX)) {
+                    System.out.println("You hit the Wumpus");
+                    // add victory screen stuff here
+                } else {
+                    System.out.println("You did not hit the wumpus");
+                    // decrease num arrows
+                    numArrows -= 1;
+                    if (numArrows == 0) {
+                        System.out.println("You ran out of arrows");
+                    }
+                }
+                shootY = moveY;
                 break;
+
             case KeyEvent.VK_RIGHT:
-                if (x < 389)
-                    x = x + 100;
+                if ((shootY) == wumpusY && (shootX + 100) == wumpusX) {
+                    System.out.println("You hit the Wumpus");
+                    // add victory screen stuff here
+                } else {
+                    System.out.println("You did not hit the wumpus");
+                    // decrease num arrows
+                    numArrows -= 1;
+                    if (numArrows == 0) {
+                        System.out.println("You ran out of arrows");
+                    }
+                }
+                shootY = moveY;
                 break;
         }
 
